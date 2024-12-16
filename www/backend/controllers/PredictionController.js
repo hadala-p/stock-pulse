4d50af78f3bc4361abec6eb31d17e952cf51ed81
Mateusz <jachow.mateusz@gmail.com>
2024-12-16 21:23:41 +0100

    Add daily prediction model

--- www/backend/controllers/PredictionController.js
+++ www/backend/controllers/PredictionController.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const Prediction = require('../models/Prediction');
+const DailyPrediction = require('../models/DailyPrediction');
 const jwt = require('jsonwebtoken');
 const predictURL = 'http://localhost:5000/predict';
 
@@ -29,6 +30,49 @@ module.exports = {
                 return res.status(200).json({
                     status: true,
                     result: predictions,
+                });
+            })
+            .catch((err) => {
+                return res.status(500).json({
+                    status: false,
+                    error: err,
+                });
+            });
+    },
+
+    postDailyPrediction: (req, res) => {
+        const token = req.headers.authorization;
+        const user = jwt.decode(token);
+        const { companyName } = req.body;
+
+        // get yahoo data and update it
+    },
+
+    getDailyPredictionData(companyName) {
+        // get yahoo finance data an post/put it
+    },
+
+    getMyDailyPredictions: (req, res) => {
+        const token = req.headers.authorization;
+        const user = jwt.decode(token);
+
+        DailyPrediction.getDailyPredictionsOf(user)
+            .then(async (predictions) => {
+                let dailyPredictions = [];
+                await predictions.forEach(async (prediction) => {
+                    if (new Date() - prediction.lastPredictionUpdate > 12 * 60 * 60 * 1000) {
+                        await getDailyPredictionData(prediction.companyName);
+                    }
+                });
+
+                let publicPredictions = await Prediction.getPublicPredictions();
+                publicPredictions.forEach((publicPrediction) => {
+                    dailyPredictions.push(publicPredictions.find((prediction) => prediction.companyName === publicPrediction.companyName));
+                });
+
+                return res.status(200).json({
+                    status: true,
+                    result: dailyPredictions,
                 });
             })
             .catch((err) => {
--- www/backend/models/index.js
+++ www/backend/models/index.js
@@ -1,12 +1,15 @@
 const User = require("./User");
 const Prediction = require("./Prediction");
+const DailyPrediction = require("./DailyPrediction");
 
 module.exports = {
     initialize(sequelize) {
         const userModel = User.initialize(sequelize);
         const predictionModel = Prediction.initialize(sequelize);
+        const dailyPredictionModel = DailyPrediction.initialize(sequelize);
 
         userModel.hasMany(predictionModel);
         predictionModel.belongsTo(userModel);
+        dailyPredictionModel.belongsTo(userModel);
     }
 };
